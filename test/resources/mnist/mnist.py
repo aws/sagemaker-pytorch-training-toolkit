ee519dfe5ae47ced42c532e4ab952efaa7b3ddfa
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-03-30 14:40:16 -0700

    Add license headers to all source files. (#4)

--- test/resources/mnist/mnist.py
+++ test/resources/mnist/mnist.py
@@ -1,4 +1,15 @@
-# Based on https://github.com/pytorch/examples/blob/master/mnist/main.py
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
 import logging
 import os
 import torch
@@ -15,6 +26,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+# Based on https://github.com/pytorch/examples/blob/master/mnist/main.py
 class Net(nn.Module):
     def __init__(self):
         logger.info("Create neural network module")
