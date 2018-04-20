86a9df46e2d08109af495bab579215765e565065
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-04-20 01:25:41 -0700

    Fix single host training test.

--- test/integration/local/test_single_machine_training.py
+++ test/integration/local/test_single_machine_training.py
@@ -17,7 +17,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-mnist_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'resources', 'mnist'))
+mnist_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'resources', 'mnist'))
 data_dir = os.path.join(mnist_path, 'data')
 
 training_dir = os.path.join(data_dir, 'training')
