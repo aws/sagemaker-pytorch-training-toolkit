c6ecd866bed45d7756feaa4999738a47e505490b
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-05-02 17:01:20 -0700

    Add correct extension when saving the model. (#16)

--- test/resources/mnist/mnist_1d.py
+++ test/resources/mnist/mnist_1d.py
@@ -41,6 +41,6 @@ class Net(nn.Module):
 
 def model_fn(model_dir):
     model = torch.nn.DataParallel(Net())
-    with open(os.path.join(model_dir, 'model'), 'rb') as f:
+    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
         model.load_state_dict(torch.load(f))
     return model
