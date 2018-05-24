20e34ce477fe1d004701e5075854436eb566767e
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-05-23 23:42:56 -0700

    Run hostname change only in training.

--- lib/start_with_right_hostname.sh
+++ lib/start_with_right_hostname.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-CURRENT_HOST=$(jq .current_host  /opt/ml/input/config/resourceconfig.json)
+if [ $1 == 'train' ]
+then
+    CURRENT_HOST=$(jq .current_host  /opt/ml/input/config/resourceconfig.json)
 
-sed -ie "s/PLACEHOLDER_HOSTNAME/$CURRENT_HOST/g" changehostname.c
+    sed -ie "s/PLACEHOLDER_HOSTNAME/$CURRENT_HOST/g" changehostname.c
 
-gcc -o changehostname.o -c -fPIC -Wall changehostname.c
-gcc -o libchangehostname.so -shared -export-dynamic changehostname.o -ldl
+    gcc -o changehostname.o -c -fPIC -Wall changehostname.c
+    gcc -o libchangehostname.so -shared -export-dynamic changehostname.o -ldl
 
-LD_PRELOAD=/libchangehostname.so python -m pytorch_container.start $@
+    LD_PRELOAD=/libchangehostname.so python -m pytorch_container.start $@
+else
+    python -m pytorch_container.start $@
+fi
