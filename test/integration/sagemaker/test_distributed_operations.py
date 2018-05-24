ba542149630a5c5550d03019cc9a5da0e88f3cab
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-05-24 16:16:16 -0700

    Adding absolute_import.

--- test/integration/sagemaker/test_distributed_operations.py
+++ test/integration/sagemaker/test_distributed_operations.py
@@ -10,6 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+from __future__ import absolute_import
 import pytest
 from test.integration import dist_operations_path
 from test.integration.sagemaker.estimator import PytorchTestEstimator
