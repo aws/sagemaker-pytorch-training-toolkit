9ee5f2dee9bb93a97c1da9c1490d8aadb6e06657
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-06-13 13:13:53 -0700

    Switch to a newest SC version. (#31)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
 
-    install_requires=['numpy', 'sagemaker-containers==2.0.3', 'torch==0.4.0', 'retrying', 'six'],
+    install_requires=['numpy', 'sagemaker-containers==2.0.4', 'torch==0.4.0', 'retrying', 'six'],
     extras_require={
         'test': ['tox', 'flake8', 'coverage', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock', 'Flask', 'boto3>=1.4.8',
                  'docker-compose', 'nvidia-docker-compose', 'sagemaker', 'PyYAML', 'torchvision']
