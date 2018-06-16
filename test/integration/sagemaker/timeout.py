f1a1cec90a93d0495dcdd4ef2a10920e02fcecdc
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-06-15 23:21:00 -0700

    Clean up repo.

--- test/integration/sagemaker/timeout.py
+++ test/integration/sagemaker/timeout.py
@@ -1,3 +1,15 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
 # TODO: this is used in all containers and sdk. We should move it to container support or sdk test utils.
 from __future__ import absolute_import
 import signal
