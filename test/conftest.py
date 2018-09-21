06c6f61489dd44768d6119652f581c7777f1ed4b
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-09-21 14:23:15 -0700

    Switch version to 1.0.0.dev.

--- test/conftest.py
+++ test/conftest.py
@@ -43,7 +43,7 @@ def pytest_addoption(parser):
     parser.addoption('--install-container-support', '-C', action="store_true")
     parser.addoption('--docker-base-name', default='pytorch')
     parser.addoption('--region', default='us-west-2')
-    parser.addoption('--framework-version', default='1.0.0')
+    parser.addoption('--framework-version', default='1.0.0.dev')
     parser.addoption('--py-version', choices=['2', '3'], default=str(sys.version_info.major))
     parser.addoption('--processor', choices=['gpu', 'cpu'], default='cpu')
     # If not specified, will default to {framework-version}-{processor}-py{py-version}
