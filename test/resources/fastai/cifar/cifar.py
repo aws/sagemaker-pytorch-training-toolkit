db3a91a2b6f46afe0ee567f3851261b3563f4cbb
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-12-28 15:03:10 -0800

    Reduce number of epochs.

--- test/resources/fastai/cifar/cifar.py
+++ test/resources/fastai/cifar/cifar.py
@@ -26,6 +26,6 @@ def main(gpu:Param("GPU to run on", str)=None):
     if gpu is None: learn.model = nn.DataParallel(learn.model)
     else: learn.distributed(gpu)
     learn.to_fp16()
-    learn.fit_one_cycle(35, 3e-3, wd=0.4)
+    learn.fit_one_cycle(2, 3e-3, wd=0.4)
     learn.save(name='model')
     print('Done - Yay!!!!!!')
