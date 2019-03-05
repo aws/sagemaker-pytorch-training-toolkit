86855dccd7243b8451106e6f22090c6e14806b8b
Ignacio Quintero <nacho950@gmail.com>
2019-03-04 16:19:54 -0800

    Freeze TorchVision to 0.2.1 (#79)
    the current 1.0.0 container shipped with torchvision 0.2.1
    there is a newever release 0.2.2 which is not fully backwards compatible
    at least in the way the datasets are stored. This could impact
    customers who suddenly receive a new version of a dependency w/o any
    control over it.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
                       'torch==1.0.0'],
     extras_require={
         'test': ['boto3>=1.4.8', 'coverage', 'docker-compose', 'flake8', 'Flask', 'mock',
-                 'pytest', 'pytest-cov', 'pytest-xdist', 'PyYAML', 'sagemaker', 'torchvision',
+                 'pytest', 'pytest-cov', 'pytest-xdist', 'PyYAML', 'sagemaker', 'torchvision==0.2.1',
                  'tox']
     },
 )
