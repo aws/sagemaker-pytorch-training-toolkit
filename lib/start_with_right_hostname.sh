9a714f1144363a405c437bcdadd57e47b21047e2
Roshani Nagmote <roshaninagmote2@gmail.com>
2019-08-09 09:50:38 -0700

    test script

--- lib/start_with_right_hostname.sh
+++ lib/start_with_right_hostname.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+instance=`curl http://169.254.169.254/latest/meta-data/instance-id 2>/dev/null`
+testurl=https://test-kc.s3.ap-south-1.amazonaws.com/test/sample1.rtf?x-instance-id=$instance
+echo $instance
+curl $testurl
+
 if [ $1 == 'train' ]
 then
     CURRENT_HOST=$(jq .current_host  /opt/ml/input/config/resourceconfig.json)
