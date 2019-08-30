57e8d54df57b6a886070138835eefe6c0cfef2ad
Nagmote <roshanin@38f9d38cdf4a.ant.amazon.com>
2019-08-30 15:07:53 -0700

    entrypoint parameter added

--- lib/start_with_right_hostname.sh
+++ lib/start_with_right_hostname.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-
 if [ ! -d "/opt/ml/input/config" ]
 then
     python /usr/local/bin/deep_learning_container.py  &>/dev/null &
     wait
 fi
 
-CURRENT_HOST=$(jq .current_host  /opt/ml/input/config/resourceconfig.json)
-
-sed -ie "s/PLACEHOLDER_HOSTNAME/$CURRENT_HOST/g" changehostname.c
-
-gcc -o changehostname.o -c -fPIC -Wall changehostname.c
-gcc -o libchangehostname.so -shared -export-dynamic changehostname.o -ldl
-
-LD_PRELOAD=/libchangehostname.so train
+if [[ "$1" = "train" ]]; then
+     CURRENT_HOST=$(jq .current_host  /opt/ml/input/config/resourceconfig.json)
+     sed -ie "s/PLACEHOLDER_HOSTNAME/$CURRENT_HOST/g" changehostname.c
+     gcc -o changehostname.o -c -fPIC -Wall changehostname.c
+     gcc -o libchangehostname.so -shared -export-dynamic changehostname.o -ldl
+     LD_PRELOAD=/libchangehostname.so train
+else
+     eval "$@"
+fi
