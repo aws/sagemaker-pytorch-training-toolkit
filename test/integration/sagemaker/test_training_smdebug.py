e742f2350755950773238488428d1ad35caf5277
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2020-01-09 11:30:02 -0800

    update: Update buildspec for PyTorch 1.3.1 (#146)
    * Modify dockerfiles for new build context
    
    * Change path for deep learning container script
    
    * Change build context to folder containing dockerfiles
    
    * Change build context in buildspec.yml

--- test/integration/sagemaker/test_training_smdebug.py
+++ test/integration/sagemaker/test_training_smdebug.py
@@ -22,7 +22,7 @@ from test.integration.sagemaker.timeout import timeout
 @pytest.mark.skip_py2_containers
 def test_training_smdebug(sagemaker_session, ecr_image, instance_type):
     hyperparameters = {'random_seed': True, 'num_steps': 50, 'smdebug_path': '/opt/ml/output/tensors', 'epochs': 1,
-                       'data_dir':training_dir}
+                       'data_dir': training_dir}
 
     with timeout(minutes=DEFAULT_TIMEOUT):
         pytorch = PyTorch(entry_point=smdebug_mnist_script,
