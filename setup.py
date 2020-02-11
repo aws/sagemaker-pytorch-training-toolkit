97e611b4cb2df13d966d508e56d1c990439b2163
Abhinav Sharma <abhinav0301@gmail.com>
2020-02-10 19:39:41 -0800

    Adding changes for PyTorch 1.4.0 DLC (#158)
    * adding 1.4 dockerfiles
    
    * update setup.py and buildspec
    
    * fix s3 paths
    
    * fix s3 links
    
    * adding future package to fix import error py2 cpu
    
    * setting final s3 paths
    
    * update package name
    
    * unpin awscli
    
    * downgrading sphinx_websupport for py2

--- setup.py
+++ setup.py
@@ -49,10 +49,10 @@ setup(
     ],
 
     install_requires=['numpy==1.16.4', 'Pillow==6.2.0', 'retrying==1.3.3', 'sagemaker-containers>=2.6.2',
-                      'six==1.12.0', 'torch==1.3.1'],
+                      'six==1.12.0', 'torch==1.4.0'],
     extras_require={
         'test': ['boto3==1.9.169', 'coverage==4.5.3', 'docker-compose==1.23.2', 'flake8==3.7.7', 'Flask==1.1.1',
                  'mock==2.0.0', 'pytest==4.4.0', 'pytest-cov==2.7.1', 'pytest-xdist==1.28.0', 'PyYAML==3.10',
-                 'sagemaker==1.28.1', 'torchvision==0.4.2', 'tox==3.7.0', 'requests_mock==1.6.0']
+                 'sagemaker==1.28.1', 'torchvision==0.5.0', 'tox==3.7.0', 'requests_mock==1.6.0']
     },
 )
