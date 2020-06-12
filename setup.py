db9e837874795b27fb90b02e1e8a45d9de27501a
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2020-06-12 16:28:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(fname):
 
 
 test_dependencies = ['boto3', 'coverage', 'docker-compose', 'flake8', 'future', 'mock',
-                     'pytest', 'pytest-cov', 'pytest-xdist', 'sagemaker==1.62.0',
+                     'pytest', 'pytest-cov', 'pytest-xdist', 'sagemaker==1.28.1',
                      'torch==1.4.0', 'torchvision==0.5.0', 'tox']
 
 if sys.version_info.major > 2:
